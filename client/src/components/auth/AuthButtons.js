c6dcf66b6e0728f7cf469b2bf483cc8f8c6a95a8
Sosuke Brause <brosuke89@gmail.com>
2020-09-17 20:11:18 -0700

    pulling julia's latet

--- client/src/components/auth/AuthButtons.js
+++ client/src/components/auth/AuthButtons.js
@@ -1,12 +1,11 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
-import { Button } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import { Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import { useUserContext } from "../context/UserContext";
 
-
 const buttonStyle = {
-  marginLeft: '10px',
+  marginLeft: "10px",
 };
 
 const useStyles = makeStyles({
@@ -14,16 +13,15 @@ const useStyles = makeStyles({
     background: "#cd3239",
     borderRadius: 15,
     border: 0,
-    color: 'white',
+    color: "white",
     height: 33,
-    padding: '0 15px',
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    padding: "0 15px",
+    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
   },
   label: {
-    textTransform: 'capitalize',
+    textTransform: "capitalize",
   },
 });
-
 
 const AuthButtons = () => {
   const classes = useStyles();
@@ -47,36 +45,64 @@ const AuthButtons = () => {
   return (
     <nav className="auth-options" style={{ marginTop: "10px" }}>
       {userData.user ? (
-        <>     <Button classes={{
-          root: classes.root, // class name, e.g. `classes-nesting-root-x`
-          label: classes.label, // class name, e.g. `classes-nesting-label-x`
-        }} style={{ float: 'right', marginRight: "20px" }} variant="contained" size="small" color="primary" onClick={logout}>
-          Log Out
-    </Button>
-          <Button classes={{
-            root: classes.root, // class name, e.g. `classes-nesting-root-x`
-            label: classes.label, // class name, e.g. `classes-nesting-label-x`
-          }} style={{ float: 'right', marginRight: "20px" }} variant="contained" size="small" color="primary" onClick={profile}>
-            Profile
-     </Button>
-        </>
-      ) : (
-          <>
-            <Button variant="contained" color="secondary" size="small" style={buttonStyle} onClick={register}>
-              Register
-    </Button>
-
-            <Button classes={{
+        <>
+          {" "}
+          <Button
+            classes={{
               root: classes.root, // class name, e.g. `classes-nesting-root-x`
               label: classes.label, // class name, e.g. `classes-nesting-label-x`
-            }} variant="contained" color="secondary" size="small" style={buttonStyle} onClick={login}>
-              Log in
-    </Button>
-          </>
-        )}
+            }}
+            style={{ float: "right", marginRight: "20px" }}
+            variant="contained"
+            size="small"
+            color="primary"
+            onClick={logout}
+          >
+            Log Out
+          </Button>
+          <Button
+            classes={{
+              root: classes.root, // class name, e.g. `classes-nesting-root-x`
+              label: classes.label, // class name, e.g. `classes-nesting-label-x`
+            }}
+            style={{ float: "right", marginRight: "20px" }}
+            variant="contained"
+            size="small"
+            color="primary"
+            onClick={profile}
+          >
+            Profile
+          </Button>
+        </>
+      ) : (
+        <>
+          <Button
+            variant="contained"
+            color="secondary"
+            size="small"
+            style={buttonStyle}
+            onClick={register}
+          >
+            Register
+          </Button>
+
+          <Button
+            classes={{
+              root: classes.root, // class name, e.g. `classes-nesting-root-x`
+              label: classes.label, // class name, e.g. `classes-nesting-label-x`
+            }}
+            variant="contained"
+            color="secondary"
+            size="small"
+            style={buttonStyle}
+            onClick={login}
+          >
+            Log in
+          </Button>
+        </>
+      )}
     </nav>
   );
 };
 
 export default AuthButtons;
-
